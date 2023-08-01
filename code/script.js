4f22d72a7d9ca7fc676b66906581f17152ac1947
Sr.Lechuga <jonattanlima@gmail.com>
2023-07-31 23:30:03 -0300

    Skin selector

--- code/script.js
+++ code/script.js
@@ -1,1 +1,20 @@
 "use strict";
+
+const zone = document.querySelector(".test-zone");
+
+zone.addEventListener("dragover",(e) => {
+    e.preventDefault();
+});
+
+zone.addEventListener("drop",(e)=>{
+    let n = e.dataTransfer.getData("skin");
+    zone.style.background = `url("../img/skin${n}.jpg")`;
+});
+
+for (let i = 1; i < document.querySelector(".skins").children.length+1; i++) {
+    document.querySelector(`.skin-${i}`).addEventListener("dragstart",(e) => changeSkin(i,e))
+};
+
+const changeSkin = (n,e) => {
+    e.dataTransfer.setData("skin",n)
+};
